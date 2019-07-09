6ef986cdc3eb217564972ba7a03c17d9ed2815dd
yuk1h1ra <koheishoji913@gmail.com>
2019-07-09 23:49:56 +0900

    abc125 c re

--- abc125/c/main.cpp
+++ abc125/c/main.cpp
@@ -18,8 +18,27 @@ int main() {
   int N;
   cin >> N;
 
-  vector<int> A(N);
-  for (int i = 0; i < N; i++) {
+  // わかりずらいので、indexを１スタートにする
+  vector<int> A(N + 2);
+  for (int i = 1; i <= N; i++) {
     cin >> A[i];
   }
+  A[0] = A[1];
+  A[N + 1] = A[N];
+
+  vector<pair<int, int>> gcdA(N + 1);
+  for (int i = 1; i <= N; i++) {
+    gcdA[i].first = gcd(A[i], gcdA[i - 1].first);
+    gcdA[N + 1 - i].second = gcd(A[N + 1 - i], gcdA[N + 2 - i].second);
+  }
+  gcdA[0].first = gcdA[2].second;
+  gcdA[N + 1].second = gcdA[N - 1].first;
+
+  int maxGcd = 0;
+  for (int i = 1; i <= N; i++) {
+    int indexGcd = gcd(gcdA[i - 1].first, gcdA[i + 1].second);
+    maxGcd = max(indexGcd, maxGcd);
+  }
+
+  cout << maxGcd << endl;
 }
