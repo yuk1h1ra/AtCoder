5e9d3500c11151857feea4c1e0ec6607c45c0b06
yuk1h1ra <koheishoji913@gmail.com>
2019-07-10 21:05:51 +0900

    abc124 d

--- abc124/d/main.cpp
+++ abc124/d/main.cpp
@@ -1,21 +1,39 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int N, K;
-    cin >> N >> K;
+  int N, K;
+  cin >> N >> K;
 
-    string S;
-    cin >> S;
+  string S;
+  cin >> S;
 
-    vector<string> splited_v;
-    getline(S, splited_v, '1');
+  vector<int> nums;
+  int tmp = 1;
+  int cnt = 0;
+  for (int i = 0; i < N; i++) {
+    if (S[i] == (char)('0' + tmp)) {
+      cnt++;
+    } else {
+      nums.push_back(cnt);
+      cnt = 1;
+      tmp = 1 - tmp;
+    }
+  }
+  if (cnt != 0)
+    nums.push_back(cnt);
+  if ((int)nums.size() % 2 == 0)
+    nums.push_back(0);
 
-    cout << N << K << endl;
-    cout << S << endl;
-    cout << splited_v << endl;
+  int maxNum = 0;
+  for (int i = 0; i < (int)nums.size(); i += 2) {
+    int tmp = 0;
+    for (int j = 0; (j < K * 2 + 1) && (i + j < (int)nums.size()); j++) {
+      tmp += (int)nums[i + j];
+    }
+    maxNum = max(maxNum, tmp);
+  }
 
+  cout << maxNum << endl;
 }
