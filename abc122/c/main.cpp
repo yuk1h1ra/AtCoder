d4eef41544027787125005e778763d7ba9c60cfa
yuk1h1ra <koheishoji913@gmail.com>
2019-07-22 23:33:15 +0900

    abc122 c AC

--- abc122/c/main.cpp
+++ abc122/c/main.cpp
@@ -7,32 +7,20 @@ typedef long long ll;
 #define debug(a) cout << #a << ": " << a << endl;
 
 int main() {
-  int N, Q; cin >> N >> Q;
-  string S; cin >> S;
+  int N, Q;
+  cin >> N >> Q;
+  string S;
+  cin >> S;
 
-  vector<int> ac(0);
-  for(int i = 0; i < (int)S.size(); i++) {
-    i = (int)S.find("AC", i);
-    if (i == -1) break;
-    ac.push_back(i+1);
+  vector<int> acCnt(N + 1);
+  rep(i, N) {
+    int isAC = (S.substr(i, 2) == "AC" ? 1 : 0);
+    acCnt[i + 1] = acCnt[i] + isAC;
   }
 
   rep(i, Q) {
-    int l, r; cin >> l >> r;
-    int acl, acr;
-    rep(j, (int)ac.size()) {
-      if (ac[j] >= l) {
-        acl = j;
-        break;
-      }
-    }
-    for(int j = (int)ac.size() - 1; j >= 0; j--) {
-      if (ac[j] < r) {
-        acr = j;
-        break;
-      }
-    }
-    cout << (int)(acr - acl + 1) << endl;
+    int l, r;
+    cin >> l >> r;
+    cout << acCnt[r - 1] - acCnt[l - 1] << endl;
   }
-
 }
