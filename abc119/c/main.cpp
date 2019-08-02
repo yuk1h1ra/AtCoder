1f182e13dc3f2e21c0c9c8db78042cc06d5c2c52
yuk1h1ra <koheishoji913@gmail.com>
2019-08-02 23:53:24 +0900

    abc119 c AC

--- abc119/c/main.cpp
+++ abc119/c/main.cpp
@@ -2,22 +2,27 @@
 
 using namespace std;
 
-int main() {
-  int N, A, B, C;
-  cin >> N >> A >> B >> C;
-  vector<int> l;
-  for (int i = 0; i < N; i++) {
-    int li;
-    cin >> li;
-    l.push_back(li);
-  }
-  sort(l.begin(), l.end(), greater<int>());
+int N, A, B, C;
+int l[1001];
 
-  int ansCnt = 0;
-  // A
-  int nearAIndex = 0;
-  for (int i = 1; i < N; i++) {
-    if (abs(l[nearAIndex] - A) > abs(l[i] - A)) {
-    }
+int dfs(int cur, int a, int b, int c) {
+  if (cur == N) {
+    if (min({a, b, c}) > 0)
+      return abs(a - A) + abs(b - B) + abs(c - C) - 30;
+    else
+      return pow(10, 9);
   }
+  int useA = dfs(cur + 1, a + l[cur], b, c) + 10;
+  int useB = dfs(cur + 1, a, b + l[cur], c) + 10;
+  int useC = dfs(cur + 1, a, b, c + l[cur]) + 10;
+  int notUse = dfs(cur + 1, a, b, c);
+  return min({useA, useB, useC, notUse});
+}
+
+int main() {
+  cin >> N >> A >> B >> C;
+  for (int i = 0; i < N; i++) {
+    cin >> l[i];
+  }
+  cout << dfs(0, 0, 0, 0) << endl;
 }
